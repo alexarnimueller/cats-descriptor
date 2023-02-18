24a16dcd931a8b655ac497a5859ccb3edda40ff1
Alex Müller <alexarnimueller@protonmail.com>
2023-02-18 17:52:13 +0100

    :smile_cat: ID tracking added, optimized

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # cats
-Python implementation of the CATS molecular descriptor
+Python implementation of the CATS molecular descriptor.
 
+## Installation
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+You can either import the function `cats_descriptor` from `cats.py` or directly call `cats.py` on a text file containing molecule IDs and SMILES strings. See `mols.txt`for an example.
+
+Using `cats_descriptor` in Python:
+```python
+from cats import cats_descriptor
+
+d = cats_descriptor(my_molecule_list)
+```
+
+Or directly from the command line:
+
+```bash
+python cats.py mols.txt
+```
+
+To get descriptions for all possible options, run:
+
+```bash
+python cats.py --help
+```
--- cats.py
+++ cats.py
@@ -1,22 +1,27 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import click
+from time import time
 import numpy as np
+import pandas as pd
+from typing import Union, List
 from multiprocessing import Process, Queue, cpu_count
-from rdkit.Chem import ChemicalFeatures
+from rdkit.Chem import ChemicalFeatures, PandasTools
+from rdkit.Chem.rdchem import Mol
 from rdkit.DataStructs import ConvertToNumpyArray
 from rdkit.Chem.Pharm2D import SigFactory, Generate
 from features import fdef_cats, fdef_rdkit
 
 
-def get_cats_factory(features='cats', names=False):
-    """ Get the feature combinations paired to all possible distances
+def get_cats_factory(features: str = "cats", names: bool = False) -> SigFactory.SigFactory:
+    """Get the feature combinations paired to all possible distances
 
     :param features: {str} which pharmacophore features to consider; available: ["cats", "rdkit"]
     :param names: {bool} whether to return an array describing the bits with names of features and distances
     :return: RDKit signature factory to be used for 2D pharmacophore fingerprint calculation
     """
-    if features == 'cats':
+    if features == "cats":
         fdef = fdef_cats
     else:
         fdef = fdef_rdkit
@@ -31,10 +36,11 @@ def get_cats_factory(features='cats', names=False):
         return sigfactory
 
 
-def _cats_corr(mols, q):
-    """ private cats descriptor function to be used in multiprocessing
+def _cats_corr(mols: List[Mol], ids: list, q: Queue):
+    """private cats descriptor function to be used in multiprocessing
 
     :param mols: {list/array} molecules (RDKit mol) to calculate the descriptor for
+    :param ids: {list/array} IDs to match calculated descriptors to input molecules
     :param q: {queue} multiprocessing queue instance
     :return: {numpy.ndarray} calculated descriptor vectors
     """
@@ -43,13 +49,13 @@ def _cats_corr(mols, q):
     for mol in mols:
         arr = np.zeros((1,))
         ConvertToNumpyArray(Generate.Gen2DFingerprint(mol, factory), arr)
-        scale = np.array([10 * [sum(arr[i:i + 10])] for i in range(0, 210, 10)]).flatten()
+        scale = np.array([10 * [sum(arr[i : i + 10])] for i in range(0, 210, 10)]).flatten()
         fps.append(np.divide(arr, scale, out=np.zeros_like(arr), where=scale != 0))
-    q.put(np.array(fps).reshape((len(mols), 210)).astype('float32'))
+    q.put((ids, np.array(fps).reshape((len(mols), 210)).astype("float32")))
 
 
-def _one_cats(mol):
-    """ Function to calculate the CATS pharmacophore descriptor for one molecule.
+def _one_cats(mol: Mol) -> np.array:
+    """Function to calculate the CATS pharmacophore descriptor for one molecule.
     Descriptions of the individual features can be obtained from the function ``get_cats_sigfactory``.
 
     :param mol: {RDKit molecule} molecule to calculate the descriptor for
@@ -58,46 +64,66 @@ def _one_cats(mol):
     factory = get_cats_factory()
     arr = np.zeros((1,))
     ConvertToNumpyArray(Generate.Gen2DFingerprint(mol, factory), arr)
-    scale = np.array([10 * [sum(arr[i:i + 10])] for i in range(0, 210, 10)]).flatten()
-    return np.divide(arr, scale, out=np.zeros_like(arr), where=scale != 0).astype('float32')
+    scale = np.array([10 * [sum(arr[i : i + 10])] for i in range(0, 210, 10)]).flatten()
+    return np.divide(arr, scale, out=np.zeros_like(arr), where=scale != 0).astype("float32")
 
 
-def cats_descriptor(mols):
-    """ Function to calculate the CATS pharmacophore descriptor for a set of molecules.
+def cats_descriptor(mols: List[Mol], ids: Union[list, None] = None) -> np.array:
+    """Function to calculate the CATS pharmacophore descriptor for a set of molecules.
     Descriptions of the individual features can be obtained from the function ``get_cats_sigfactory``.
 
     :param mols: {list/array} molecules (RDKit mol) to calculate the descriptor for
     :return: {numpy.ndarray} calculated descriptor vectors
     """
+    if ids is None:
+        ids = list(range(len(mols)))
+    assert len(ids) == len(mols), "Number of IDs don't match number of molecules!"
+
     queue = Queue()
-    rslt = []
-    if len(mols) < 4 * cpu_count():  # if only small array, don't parallelize
-        for mol in mols:
-            rslt.append(_one_cats(mol))
+
+    # if only small array, don't parallelize
+    if len(mols) < 4 * cpu_count():
+        rslt = pd.DataFrame(map(_one_cats, mols), columns=[f"CATS{i}" for i in range(1, 211)])
+        rslt["ID"] = ids
+
+    # if many molecules, do parallelize
     else:
-        for m in np.array_split(np.array(mols), cpu_count()):
-            p = Process(target=_cats_corr, args=(m, queue,))
+        rslt = pd.DataFrame()
+        for i, m in zip(np.array_split(np.array(ids), cpu_count()), np.array_split(np.array(mols), cpu_count())):
+            p = Process(target=_cats_corr, args=(m, i, queue))
             p.start()
         for _ in range(cpu_count()):
-            rslt.extend(queue.get(10))
-    return np.array(rslt).reshape((len(mols), 210)).astype('float32')
+            ids, desc = queue.get(10)
+            desc = pd.DataFrame(desc, columns=[f"CATS{i}" for i in range(1, 211)])
+            desc["ID"] = ids
+            rslt = pd.concat((rslt, desc))
+    return rslt[["ID"] + [f"CATS{i}" for i in range(1, 211)]].sort_values("ID")
+
+
+@click.command()
+@click.argument("smiles_file")
+@click.option("-o", "--output_file", dest="output_file", type=str, default="cats.txt", help="output filename")
+@click.option("-i", "--id_column", dest="id_column", type=str, default="ID", help="column header of the ID column")
+@click.option(
+    "-s", "--smiles_column", dest="smiles_column", type=str, default="SMILES", help="column header of the SMILES column"
+)
+@click.option("-v", "--verbose", dest="verbose", is_flag=True, default=False, help="verbosity")
+def run(smiles_file: str, output_file: str, id_column: str, smiles_column: str, verbose: bool):
+    if verbose:
+        print("Loading molecules...")
+    df = pd.read_csv(smiles_file, delimiter="\t")
+    PandasTools.AddMoleculeColumnToFrame(df, smiles_column, "Molecule", includeFingerprints=False)
+    if verbose:
+        print("Calculating descriptor...")
+    start = time()
+    d = cats_descriptor(df.Molecule, ids=df[id_column])
+    t = time() - start
+    if verbose:
+        print(f"\nShape of descriptor: {d.shape}")
+        print(f"\nCalculation took {t:.3f}sec for {len(df)} molecules")
+        print(f"\nSaving descriptor to output file {output_file}")
+    d.to_csv(output_file, sep="\t")
 
 
 if __name__ == "__main__":
-    from rdkit.Chem import MolFromSmiles
-    from time import time
-
-    smls = list()
-    with open('./mols.csv', 'r') as f:
-        for line in f:
-            smls.append(line.strip())
-    print("Loading molecules...")
-    molecules = [MolFromSmiles(s) for s in smls]
-
-    print("Calculating descriptor...")
-    start = time()
-    d = cats_descriptor(molecules)
-    t = time() - start
-    print(d)
-    print("Shape of descriptor: %s" % str(d.shape))
-    print("\nCalculation took %.4f for %i molecules" % (t, len(molecules)))
+    run()
